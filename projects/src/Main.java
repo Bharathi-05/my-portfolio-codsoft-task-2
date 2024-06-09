91ab9bc0ee0c51cdca79ccf615df8886bb01429c
Bharathi-05 <124319979+Bharathi-05@users.noreply.github.com>
2024-06-09 19:49:43 +0530

    Add files via upload

---
+++ projects/src/Main.java
@@ -0,0 +1,18 @@
+import java.net.SocketOption;
+import java.util.Arrays;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+        }
+        static int sum(int a,int b){
+        int sum1 =a+b;
+        return sum1 ;
+    }
+}
